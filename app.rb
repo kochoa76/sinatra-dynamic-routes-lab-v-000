c4a19b12446ca3b72871aa23453ff655b723e2a3
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-07-15 17:54:29 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -22,7 +22,12 @@ class App < Sinatra::Base
    answer
  end
 
- get '/say/:word1/:word2/:word3/:word4/:word5'
+ get '/say/:word1/:word2/:word3/:word4/:word5' do 
+
+   answer = ''
+
+   params[:word1][:word2][:word3][:word4][:word5] 
+ end 
 
 
 end
