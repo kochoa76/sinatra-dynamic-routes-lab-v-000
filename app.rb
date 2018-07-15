7254e662dd8d9da95ed7c78b785e7b0d00f15bf3
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-07-15 17:40:03 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -15,9 +15,9 @@ class App < Sinatra::Base
   get '/say/number/:phrase' do
     answer= ''
 
-  params[:number].to_i.times do
-    answer+=params[:phrase]
-  end
+    params[:number].to_i.times do
+      answer += params[:phrase]
+    end
   answer
 end
 
